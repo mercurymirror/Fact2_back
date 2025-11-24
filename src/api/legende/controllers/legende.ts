a994bb4eacbc4afb687d6d405612bad4b6f3ea12
mercurymirror <romainpruvost85@gmail.com>
2025-11-24 21:43:27 +0100

    legende controller

--- src/api/legende/controllers/legende.ts
+++ src/api/legende/controllers/legende.ts
@@ -4,4 +4,25 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::legende.legende');
+export default factories.createCoreController('api::legende.legende', () => ({
+  async find(ctx) {
+    // Force le populate profond pour récupérer le glossaire avec les illustrations
+    ctx.query = {
+      ...ctx.query,
+      populate: {
+        layout: {
+          populate: {
+            dico: {
+              populate: {
+                illustration: true,
+              },
+            },
+          },
+        },
+      },
+    };
+
+    const { data, meta } = await super.find(ctx);
+    return { data, meta };
+  },
+}));
