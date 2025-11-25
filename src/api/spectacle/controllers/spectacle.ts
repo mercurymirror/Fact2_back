ef0620e45e962e32afb38fbe2d0a6adf0c1bca5f
mercurymirror <romainpruvost85@gmail.com>
2025-11-25 12:27:49 +0100

    UPDATE controller for plusquunepiece

--- src/api/spectacle/controllers/spectacle.ts
+++ src/api/spectacle/controllers/spectacle.ts
@@ -4,4 +4,46 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::spectacle.spectacle');
+export default factories.createCoreController('api::spectacle.spectacle', ({ strapi }) => ({
+  async find(ctx) {
+    // Populate profond automatique pour plus_qu_une_pieces
+    ctx.query = {
+      ...ctx.query,
+      populate: {
+        cast: true,
+        image: true,
+        gallery: true,
+        plus_qu_une_pieces: {
+          populate: {
+            cast: true,
+            gallery: true,
+          },
+        },
+      },
+    };
+
+    const { data, meta } = await super.find(ctx);
+    return { data, meta };
+  },
+
+  async findOne(ctx) {
+    // Populate profond automatique pour plus_qu_une_pieces
+    ctx.query = {
+      ...ctx.query,
+      populate: {
+        cast: true,
+        image: true,
+        gallery: true,
+        plus_qu_une_pieces: {
+          populate: {
+            cast: true,
+            gallery: true,
+          },
+        },
+      },
+    };
+
+    const { data, meta } = await super.findOne(ctx);
+    return { data, meta };
+  },
+}));
