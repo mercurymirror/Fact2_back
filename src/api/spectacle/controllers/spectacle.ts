d39196d6c3dafe349e72dd19027d49e98f4b39aa
mercurymirror <romainpruvost85@gmail.com>
2025-12-08 13:13:42 +0100

    UPDATE spectacles controller

--- src/api/spectacle/controllers/spectacle.ts
+++ src/api/spectacle/controllers/spectacle.ts
@@ -4,44 +4,35 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::spectacle.spectacle', ({ strapi }) => ({
+// Configuration centralisée du populate
+const DEFAULT_POPULATE = {
+  cast: true,
+  image: true,
+  gallery: true,
+  plus_qu_une_pieces: {
+    populate: {
+      cast: true,
+      gallery: true,
+    },
+  },
+};
+
+export default factories.createCoreController('api::spectacle.spectacle', () => ({
   async find(ctx) {
-    // Populate profond automatique pour plus_qu_une_pieces
-    ctx.query = {
-      ...ctx.query,
-      populate: {
-        cast: true,
-        image: true,
-        gallery: true,
-        plus_qu_une_pieces: {
-          populate: {
-            cast: true,
-            gallery: true,
-          },
-        },
-      },
-    };
+    // Appliquer le populate par défaut seulement si aucun populate n'est spécifié
+    if (!ctx.query.populate) {
+      ctx.query.populate = DEFAULT_POPULATE;
+    }
 
     const { data, meta } = await super.find(ctx);
     return { data, meta };
   },
 
   async findOne(ctx) {
-    // Populate profond automatique pour plus_qu_une_pieces
-    ctx.query = {
-      ...ctx.query,
-      populate: {
-        cast: true,
-        image: true,
-        gallery: true,
-        plus_qu_une_pieces: {
-          populate: {
-            cast: true,
-            gallery: true,
-          },
-        },
-      },
-    };
+    // Appliquer le populate par défaut seulement si aucun populate n'est spécifié
+    if (!ctx.query.populate) {
+      ctx.query.populate = DEFAULT_POPULATE;
+    }
 
     const { data, meta } = await super.findOne(ctx);
     return { data, meta };
