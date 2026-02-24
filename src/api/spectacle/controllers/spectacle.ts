6f8c0901d9ff2b0d792cf77b644075f6314ce138
mercurymirror <romainpruvost85@gmail.com>
2026-02-24 15:10:40 +0100

    ADD populate video in spectacle

--- src/api/spectacle/controllers/spectacle.ts
+++ src/api/spectacle/controllers/spectacle.ts
@@ -2,13 +2,14 @@
  * spectacle controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
 // Configuration centralisée du populate
 const DEFAULT_POPULATE = {
   cast: true,
   image: true,
   gallery: true,
+  video: true,
   dossier_de_diffusion: true,
   plus_qu_une_pieces: {
     populate: {
@@ -18,24 +19,27 @@ const DEFAULT_POPULATE = {
   },
 };
 
-export default factories.createCoreController('api::spectacle.spectacle', () => ({
-  async find(ctx) {
-    // Appliquer le populate par défaut seulement si aucun populate n'est spécifié
-    if (!ctx.query.populate) {
-      ctx.query.populate = DEFAULT_POPULATE;
-    }
+export default factories.createCoreController(
+  "api::spectacle.spectacle",
+  () => ({
+    async find(ctx) {
+      // Appliquer le populate par défaut seulement si aucun populate n'est spécifié
+      if (!ctx.query.populate) {
+        ctx.query.populate = DEFAULT_POPULATE;
+      }
 
-    const { data, meta } = await super.find(ctx);
-    return { data, meta };
-  },
+      const { data, meta } = await super.find(ctx);
+      return { data, meta };
+    },
 
-  async findOne(ctx) {
-    // Appliquer le populate par défaut seulement si aucun populate n'est spécifié
-    if (!ctx.query.populate) {
-      ctx.query.populate = DEFAULT_POPULATE;
-    }
+    async findOne(ctx) {
+      // Appliquer le populate par défaut seulement si aucun populate n'est spécifié
+      if (!ctx.query.populate) {
+        ctx.query.populate = DEFAULT_POPULATE;
+      }
 
-    const { data, meta } = await super.findOne(ctx);
-    return { data, meta };
-  },
-}));
+      const { data, meta } = await super.findOne(ctx);
+      return { data, meta };
+    },
+  }),
+);
